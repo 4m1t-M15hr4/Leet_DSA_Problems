7bfd2a7f3a1fb312d7f732e1109ba00d7665d180
Amit-Mishra <amitmishra31929@gmail.com>
2026-09-05 20:09:42 +0530

    [3904] [Time Beats: 90.32%] [Memory Beats: 51.61%] - LeetPush

---
+++ Leet_DSA_Problems/DCP-09-26/3904-smallest-stable-index-ii/3904-Smallest-Stable-Index-II.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def firstStableIndex(self, nums, k):
+        n = len(nums)
+        max_till = nums[0]
+        max_left =  []
+        
+        for i in range(n):
+            if nums[i] > max_till:
+                max_till = nums[i]
+            max_left.append(max_till)
+
+        min_till = nums[-1]
+        min_left = []
+        for i in range(n-1, -1, -1):
+            if nums[i] < min_till:
+                min_till = nums[i]
+            min_left.append(min_till)
+        min_left = min_left[::-1]
+        for i in range(n):
+            instable = max_left[i] - min_left[i]
+            if instable <= k:
+                return i
+        return -1
