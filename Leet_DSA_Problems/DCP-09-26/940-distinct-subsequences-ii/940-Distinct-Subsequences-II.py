b180aed5e4686d1f9c6135f0aedda6343353fd77
Amit-Mishra <amitmishra31929@gmail.com>
2026-09-07 20:52:57 +0530

    [940] [Time Beats: 6.45%] [Memory Beats: 77.42%] - LeetPush

---
+++ Leet_DSA_Problems/DCP-09-26/940-distinct-subsequences-ii/940-Distinct-Subsequences-II.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def distinctSubseqII(self, s):
+        n = len(s)
+        dp = [1] * (n +1)
+        last = {}
+        
+        for i in range(n):
+            dp[i + 1] = dp[i] * 2
+            if s[i] in last: dp[i + 1] -= dp[last[s[i]]]
+            last[s[i]] = i
+
+        return (dp[-1] -1) % ( 10**9 + 7 )
