4140513a796e9a46bd4ee666c444e7fe052b74b2
Amit-Mishra <amitmishra31929@gmail.com>
2026-09-20 08:52:23 +0530

    [3498] [Time Beats: 70.00%] [Memory Beats: 55.57%] - LeetPush

---
+++ Leet_DSA_Problems/DCP-09-26/3498-reverse-degree-of-a-string/3498-Reverse-Degree-of-a-String.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        ttl = 0
+        for i, ch in enumerate(s, start= 1):
+            rev_pos= 26- (ord(ch)- ord('a'))
+            ttl += rev_pos * i
+        return ttl
+
